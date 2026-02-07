c72211bd5c40a64a686048a8683c577a9ebe3e14
Amany500 <amanyelhakly500@gmail.com>
2026-02-07 06:48:50 +0200

    Edit

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "e-commerce-production-24e0.up.railway.app/api",
 });
 
 api.interceptors.request.use((config) => {
