dc529a2f9367a7d9f10e607b71e82fdf2929d803
Amany500 <amanyelhakly500@gmail.com>
2026-02-07 06:54:14 +0200

    Edit

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL:"https://e-commerce-production-24e0.up.railway.app/api",
+  // تأكدي من وجود https:// في البداية ليعرف المتصفح أنه عنوان خارجي
+  baseURL: "https://e-commerce-production-24e0.up.railway.app/api", 
 });
 
 api.interceptors.request.use((config) => {
